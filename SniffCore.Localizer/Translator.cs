1748f1a0e76088d4a5995b788dd6ac1ff7e82b6d
David Wendland <the.devicenator@gmail.com>
2022-05-14 09:43:18 +0200

    Cleanup

--- SniffCore.Localizer/Translator.cs
+++ SniffCore.Localizer/Translator.cs
@@ -1,7 +1,7 @@
-﻿//
+﻿// 
 // Copyright (c) David Wendland. All rights reserved.
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
-//
+// 
 
 using System.Windows;
 
